e7ba6448624592b3b683f1a77e462c1bcd19b971
tanvirulhasan00 <tanvirulhasanedesh@gmail.com>
2025-12-25 11:07:45 +0600

    add application user model

---
+++ InventoryMS.Models/Entities/ApplicationUserModel/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMS.Models.Entities.ApplicationUserModel
+{
+    public class ApplicationUser
+    {
+        public string FullName { get; set; }
+        public string Password { get; set; }
+    }
+}
