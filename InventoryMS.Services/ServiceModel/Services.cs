d0720e49fc38addcd527de6799411f76b85df8ff
tanvirulhasan00 <tanvirulhasanedesh@gmail.com>
2026-01-03 14:06:25 +0600

    add service class

---
+++ InventoryMS.Services/ServiceModel/Services.cs
@@ -0,0 +1,11 @@
+﻿using InventoryMS.Services.IServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMS.Services.ServiceModel
+{
+    public class Services : IServices
+    {
+    }
+}
